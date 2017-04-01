d1e3cc27799717a7327f7ef0cc82a8f21c5c8692
Ulises Giacoman <ulises.giacoman@forrent.com>
2017-03-31 22:18:48 -0400

    Add example css

--- pages/buddy.js
+++ pages/buddy.js
@@ -4,9 +4,21 @@ export default class Buddy extends React.Component {
   render() {
     return (
       <div>
+        <style jsx>{`
+
+          h1 {
+            color: red;
+          }
+          button {
+            color: red;
+          }
+
+          `}
+        </style>
         <h1>
           Hello buddy!
         </h1>
+        <button>hi</button>
         <Transition />
       </div>
 
